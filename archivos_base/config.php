d6f008d703117bdbc9dc159fe028e8780b94d4db
Stephany <stephanygloria.21@gmail.com>
2025-06-09 19:33:13 -0400

    Archivos Base

--- archivos_base/config.php
+++ archivos_base/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost:82/mercurio';
+$config['base_url'] = 'http://localhost:8085/mercurio';
 
 $config['IDAPLICACION'] = 11;
 
